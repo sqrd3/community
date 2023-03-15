f1ce637ed7bc21b183b3bde77525cd2e9b8e9ded
sqrd3 <1944313172@qq.com>
2023-03-15 20:50:10 +0800

    7

--- community/src/components/login.vue
+++ community/src/components/login.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="wrapper">
     <div class="header">
-        <!-- <img class="img" src=""> -->
-        <div class="language">
-            <span>English</span>
+        <img class="img" src="../assets/image/gnnutop2.jpg">
+        <div class="language" @click="language">
+            <img src="../assets/image/change.png" alt="">
+            <span v-show="isEnglish==0">English</span>
+            <span v-show="isEnglish==1">中文</span>
         </div>
     </div>
     <div class="content">
-        <!-- <div class="login-left"></div> -->
+        <div class="login-left"></div>
         <div class="auth-left">
-            <img src="" alt="" class="image">
+            <img src="../assets/image/timg2.jpg" alt="" class="image">
             <div class="remark">
                 <h3 class="justify">统一身份认证</h3>
                 <ul class="mention">
@@ -18,24 +20,64 @@
                     <li>◆ 若忘记密码，请点击登录下面的忘记密码自助修改</li>
                     <li>◆ 在办理完统一身份认证相关业务后，请点击右上角的安全退出按钮</li>
                 </ul>
-                <p class="aqts">安全提示：为了您的安全，请不定期修改密码</p>
+                <p class="aqts">　　　安全提示：为了您的安全，请不定期修改密码</p>
             </div>
         </div>
         <div class="login-right">
             <div class="logodz">
-                <!-- <img src="" alt=""> -->
+                <img src="../assets/image/logo.png" alt="">
             </div>
             <div class="auth-tab">
                 <div class="tab-link">
-                    <ul>
-                        <li>账号登录</li>
-                        <li>动态码登录</li>
-                        <li>微信登录</li>
+                    <ul v-show="isEnglish==0">
+                         <li v-for="(item,index) in cn" :key="index" @click="select(index)" :class="{selected:index==selected}">{{item}}</li>
+                    </ul>
+                     <ul v-show="isEnglish==1">
+                        <li v-for="(item,index) in eg" :key="index"  @click="select(index)" :class="{selected:index==selected}">{{item}}</li>
                     </ul>
                 </div>
                 <div class="clearfloat"></div>
-                <div class="tab-content"></div>
+                <div class="tab-content">
+                    <div class="content-item" v-show="selected==0">
+                        <div class="item">
+                            <p class="lable-c" v-show="isEnglish==0">账号</p>
+                            <p class="lable-r" v-show="isEnglish==1">Account</p>
+                            <input type="text" class="inputdz" placeholder="学号" v-show="isEnglish==0"> 
+                            <input type="text" class="inputdz" placeholder="ID" v-show="isEnglish==1"> 
+                        </div>
+                        <div class="item">
+                            <p class="lable-c" v-show="isEnglish==0">密码</p>
+                            <p class="lable-r" v-show="isEnglish==1">Password</p>
+                            <!-- <input type="password" class="inputdz" placeholder="密码" v-show="isEnglish==0"> -->
+                            <el-input  prefix-icon="el-icon-lock" v-show="isEnglish==0" placeholder="请输入密码" v-model="userInfo.password"
+              show-password></el-input>
+                             <el-input  prefix-icon="el-icon-lock" v-show="isEnglish==1" placeholder="password" v-model="userInfo.password"
+              show-password></el-input>
+              <hr>
+                        </div>
+                    </div>
+                    <div class="content-item" v-show="selected==1">
+                          <div class="item">
+                            <p class="lable-c" v-show="isEnglish==0">手机号</p>
+                            <p class="lable-r" v-show="isEnglish==1">Phone number</p>
+                            <input type="text" class="inputdz" placeholder="手机号" v-show="isEnglish==0">
+                            <input type="text" class="inputdz" placeholder="Phone number" v-show="isEnglish==1"> 
+                        </div>
+                        <div class="item">
+                            <p class="lable-c" v-show="isEnglish==0">动态码</p>
+                            <p class="lable-r" v-show="isEnglish==1">Dynamic code</p>
+                            <input type="text" class="inputdz" placeholder="动态码"  v-show="isEnglish==0">
+                            <input type="text" class="inputdz" placeholder="Dynamic code"  v-show="isEnglish==1">
+                        </div>
+                    </div>
+                    <div class="content-item" v-show="selected==2"></div>
+                </div>
             </div>
+            <div class="question">
+               <input type="checkbox" name="" class="ck">近期免登录
+               <a href="">忘记密码？</a>
+            </div>
+            <div class="btn"><button class="enter">登录</button></div >
         </div>
     </div>
   </div>
@@ -43,11 +85,104 @@
 
 <script>
 export default {
+    data() {
+        return {
+           isEnglish:0,
+           cn:["账号登录","动态码登录","微信登录"],
+           eg:["Account login","Code login","Wechat login"],
+           selected:0,
+           userInfo:{
+            password:"",
+            account:""
+           }
+        }
+    },
+    methods:{
+        language(){
+            this.isEnglish==0?this.isEnglish=1:this.isEnglish=0
+        },
+        select(index){
+            this.selected=index
 
+        }
+    }
 }
 </script>
 
 <style scoped>
+.item >>>.el-input__inner {
+    border: 0;
+    border-bottom: 1px;
+  }
+.btn{
+    width: 80%;
+    text-align: center;
+    display: block;
+    margin: auto;
+    margin-top: 20px;
+}
+.enter{
+    display: block;
+    background: rgb(44,90,177);
+    color: #fff;
+    padding: 6px 12px;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1.42857143;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: middle;
+    border-radius: 3px;
+    text-decoration: none;
+    border: 1px solid transparent;
+    width: 100%;
+}
+.question a{
+    float: right;
+    margin-right: 20px;
+}
+a{
+    text-decoration: none;
+}
+.ck{
+    margin-left: 40px;
+}
+.item span{
+    position: absolute;
+    left: 80px;
+    font-size: 13px;
+    color: red;
+}
+.item input{
+    width: 100%;
+    padding-left: 15px;
+    box-sizing: border-box;
+    height: 30px;
+    border: none;
+    outline: none;
+    border-bottom: 1px solid #cdcdcd;
+}
+.item p{
+    font-size: 16px;
+    color: #000000;
+    line-height: 22px;
+    margin: 0;
+}
+.item{
+    position: relative;
+    margin-top: 0;
+    margin-bottom: 17px;
+}
+.wrapper{
+    width: 100%;
+    height: 100vh;
+}
+.auth-left{
+    width: calc(100% - 440px);
+    height: 100%;
+    float: left;
+    position: relative;
+}
 .tab-link li{
     min-width: 25%;
     margin: 0 2%;
@@ -59,7 +194,7 @@ export default {
     cursor: pointer;
 }
 .tab-link li.selected{
-    border-bottom: 2px solid #00A185;
+    border-bottom: 2px solid rgb(44,90,177);
 }
 ul{
     list-style: none;
@@ -68,7 +203,8 @@ ul{
 }
 .header{
     height: 60px;
-    background: url(../images/topbg.png);
+    /* background: url(../images/topbg.png); */
+    background: rgb(44,90,177);
     background-size: 100% 100%;
     overflow: hidden;
     position: relative;
@@ -99,25 +235,19 @@ ul{
     width: 100%;
     height: calc(100% - 60px);
 }
-/* .content .login-left{
+.content .login-left{
     width: 50%;
     height: 287px;
     float: left;
     position: relative;
     display: none;
-} */
-.auth_left{
-    width: calc(100% - 440px);
-    height: 100%;
-    float: left;
-    position: relative;
 }
-.content .login-right{
+.login-right{
     width: 440px;
     height: 100%;
     background: #fff;
     float: right;
-    box-shadow: -4px 0 8px 4px rgb(0 0 0 / 20%);
+    box-shadow: -4px 0 8px 4px rgba(0,0,0,0.20);
 }
 .image{
     height: 100%;
@@ -151,7 +281,6 @@ ul{
     list-style: none;
     padding: 0;
     display: block;
-    list-style-type: disc;
     margin-block-start: 1em;
     margin-block-end: 1em;
     margin-inline-start: 0px;
@@ -178,5 +307,12 @@ ul{
 }
 .clearfloat{
     clear: both;
+}
+.content-item{
+    position: relative;
+    padding-top: 20px;
+    max-width: 362px;
+    max-height: 300px;
+    overflow: hidden;
 }
 </style>
